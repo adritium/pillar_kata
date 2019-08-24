0f813a3882fac9acbfa4adfd06deeaca5051ca8e
Adrian Muresan <adrianmuresan@outlook.com>
2019-08-24 16:08:05 -0400

    1. add test to check adding one item to cart

--- Checkout.py
+++ Checkout.py
@@ -21,9 +21,18 @@ class Checkout:
             raise Exception
         return item.get_cost()
 
-    def remove_item(self, name):
+    def remove_item_db(self, name):
         if name in self.item_db:
             del self.item_db[name]
+
+    def encart(self, item_name, unit_amount):
+        self.cart[item_name] = unit_amount
+
+    def get_cart_total(self):
+        cost = 0
+        for item_name, unit_amount in self.cart.items():
+            cost = cost + self.get_item_cost(item_name) * unit_amount
+        return cost
 
     #def scanItem(self, name, count):
 
--- the_tests.py
+++ the_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from Checkout import Checkout
 
+
 class TestCheckoutOrder(unittest.TestCase):
     def setUp(self):
         self.Checkout = Checkout()
@@ -17,15 +18,23 @@ class TestCheckoutOrder(unittest.TestCase):
         self.Checkout.create_item_db_count("donuts (dozen)", 3)
 
     def test_createItemDbWeight(self):
-        self.Checkout.create_item_db_weight("tuna fish (weight)", 3.56)
+        self.createItemDatabase()
         tuna_fish_cost = self.Checkout.get_item_cost("tuna fish (weight)")
-        self.assertEqual(tuna_fish_cost, 3.56)
+        self.assertEqual(tuna_fish_cost, 5.24)
 
     def test_removeItemDb(self):
-        self.Checkout.create_item_db_weight("tuna fish (weight)", 3.56)
-        self.Checkout.remove_item("tuna fish (weight)")
+        self.createItemDatabase()
+        self.Checkout.remove_item_db("tuna fish (weight)")
         with self.assertRaises(Exception):
             self.Checkout.get_item_cost("tuna fish (weight)")
+
+    def test_add_one_item_to_cart(self):
+        self.createItemDatabase()
+        # Since each item is unique wrt the units of measurement, no need to specify units
+        self.Checkout.encart("tuna fish (weight)", 3)
+        cart_cost = self.Checkout.get_cart_total()
+
+        self.assertEqual(cart_cost, 5.24*3)
 
     #def test_scanItem(self):
 
